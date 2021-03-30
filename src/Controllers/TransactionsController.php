2e9e859efd16d3dbb56387a8bba8c2c80f891082
unknown <dwerlich21@gmail.com>
2021-03-29 22:56:17 -0300

    agoraFoi

--- src/Controllers/TransactionsController.php
+++ src/Controllers/TransactionsController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 
 
 use App\Helpers\Utils;
-use App\helpers\Validator;
+use App\Helpers\Validator;
 use App\Models\Entities\Countries;
 use App\Models\Entities\Deal;
 use App\Models\Entities\Transaction;
